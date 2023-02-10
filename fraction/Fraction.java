6de3c9435f6e956aedc019147604e97f63cabd05
Mongsaemong <110557730+Sirimongkol-2546@users.noreply.github.com>
2023-02-10 12:17:15 +0700

    LAB 4 OOP

---
+++ fraction/Fraction.java
@@ -0,0 +1,22 @@
+public class Fraction {
+    public int topN;
+    public int btmN;
+    
+    public String toFraction(){
+        return topN + "/" + btmN;
+    }
+    
+    public String toFloat(){
+        return "" + ((double)topN / btmN);
+    }
+
+    public void addFraction(Fraction f){
+        if (btmN == f.btmN){
+            topN = topN + f.topN;
+        }
+        else{
+            topN = (topN *  f.btmN) + (f.topN * btmN);
+            btmN = btmN * f.btmN;
+        }
+    }
+}
