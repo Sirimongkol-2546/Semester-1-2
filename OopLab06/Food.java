5a0fc89ac08bfdee283e881d8a30d7f425bb5908
Mongsaemong <110557730+Sirimongkol-2546@users.noreply.github.com>
2023-02-28 00:40:16 +0700

    LAB 06 OOP

---
+++ OopLab06/Food.java
@@ -0,0 +1,20 @@
+package OopLab06;
+
+public class Food {
+     private static int energy = 10;
+    private static double price = 50;
+    
+    public static void setPrice(double price){
+        if(Food.price < price){
+            System.out.println("Cannot update the food price.");
+        }
+    }
+    
+    public static double getPrice(){
+        return price;
+    }
+    
+    public static int getEnergy(){
+        return energy;
+    }
+}
---
+++ OopLab06/Programmer.java
@@ -0,0 +1,31 @@
+package OopLab06;
+
+public class Programmer extends Employee{
+    private int happiness;
+    
+    public void coding(String str){
+        if(this.getEnergy() >= 30){
+            this.setEnergy(this.getEnergy()-30);
+            this.setHappiness(this.getHappiness() - 30);
+            System.out.println("Your code is " + str);   
+        }
+        else{
+            this.setEnergy(this.getEnergy()-30);
+            this.setHappiness(this.getHappiness() - 30);
+            System.out.println("Error Error Error");
+        }
+        
+    }
+    
+    public void coding(char str){
+        this.coding(str + "");
+    }
+    
+    public int getHappiness(){
+        return happiness;
+    }
+    
+    public void setHappiness(int happiness){
+        this.happiness = happiness;
+    }
+}
