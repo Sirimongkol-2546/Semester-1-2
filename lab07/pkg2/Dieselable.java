be0aa510ba30c629a887b454c4ccd5a625fc9d26
Mongsaemong <110557730+Sirimongkol-2546@users.noreply.github.com>
2023-03-03 00:33:50 +0700

    LAB 07 OOP 2

---
+++ lab07/pkg2/Dieselable.java
@@ -0,0 +1,12 @@
+package lab07.pkg2;
+
+/**
+ *
+ * @author sudthirak
+ */
+public interface Dieselable {
+    
+    public abstract void startEngine();
+    public abstract void stopEngine();
+    
+}
---
+++ lab07/pkg2/Floatable.java
@@ -0,0 +1,11 @@
+package lab07.pkg2;
+
+/**
+ *
+ * @author sudthirak
+ */
+public interface Floatable {
+    
+    public abstract void fl0at();
+    
+}
