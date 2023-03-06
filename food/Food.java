ccd4b2cec183c7b8c5c8aa45c3632828df87d581
Mongsaemong <110557730+Sirimongkol-2546@users.noreply.github.com>
2023-03-06 22:07:20 +0700

    Add files via upload

---
+++ food/Food.java
@@ -0,0 +1,23 @@
+package food;
+/**
+ *
+ * @author sudthirak
+ */
+public class Food {
+    private static final int energy = 10;
+    private static double price = 50;
+    
+    public static void setPrice(double price){
+        if(Food.price < price){
+            System.out.println("Cannot update the food price.");
+        }
+    }
+    
+    public static double getPrice(){
+        return price;
+    }
+    
+    public static int getEnergy(){
+        return energy;
+    }
+}
