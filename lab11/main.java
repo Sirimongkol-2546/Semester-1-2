beb956872b18a47a1f60ed147344466c88a92352
Mongsaemong <110557730+Sirimongkol-2546@users.noreply.github.com>
2023-04-07 00:58:19 +0700

    OOP LAB 11 NOT FINAL

---
+++ lab11/main.java
@@ -0,0 +1,15 @@
+package lab11;
+
+/**
+ *
+ * @author sudthirak
+ */
+public class main {
+
+    public static void main(String[] args) {
+//        new CalculatorOneGUI();
+//        new CalculatorTwoGUI(); 
+        new TellerGUI();
+    }
+    
+}
